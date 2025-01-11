92ad0f6e87a08b786ab3301482a55a125a8c9dfd
navneet <navneetsingh813@gmail.com>
2025-01-11 15:03:19 +0530

    Initial code commit

---
+++ app/src/main/java/com/example/notesapp/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.notesapp
+
+import android.os.Bundle
+import android.view.inputmethod.InputBinding
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.notesapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var adapter: NoteAdapter
+    private lateinit var noteList: ArrayList<String>
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        adapter = NoteAdapter()
+        binding.rvNote.adapter = adapter
+
+    }
+}
